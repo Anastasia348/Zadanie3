c3cd5986b1bda7cc9d51b104386032235c677a6c
zadanie3 <anastasiazubkovskaa348@gmail.com>
2022-09-17 18:45:37 +0300

    Задание 3.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,12 @@
 public class Main {
-    public static void mail(String[]args){
-        Guitar guitar1= new Guitar(4);
-        Guitar guitar2= new Guitar(5);
-        Guitar guitar3= new Guitar(6);
-        Barabanchik barabanchik1 = new Barabanchik(14);
-        Barabanchik barabanchik2 = new Barabanchik(15);
-        Truba truba1 = new Truba(10);
-        Truba truba2 = new Truba(11);
+    public static void main(String[]args){
+        Guitar guitar1= new Guitar(3);
+        Guitar guitar2= new Guitar(4);
+        Guitar guitar3= new Guitar(5);
+        Barabanchik barabanchik1 = new Barabanchik(12);
+        Barabanchik barabanchik2 = new Barabanchik(16);
+        Truba truba1 = new Truba(11);
+        Truba truba2 = new Truba(15);
         Instruments[] instrument = {guitar1, guitar2, guitar3, barabanchik1, barabanchik2, truba1,truba2};
         for(Instruments instrument1 : instrument){
             instrument1.play();
